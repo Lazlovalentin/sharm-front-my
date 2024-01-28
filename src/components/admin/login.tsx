4027b335ee3c49fc339768d7ea339e84515a6391
Oleg Tkach <KratosHome101@gmail.com>
2024-01-28 11:06:54 +0200

    midelwear4

--- src/components/admin/login.tsx
+++ src/components/admin/login.tsx
@@ -1,10 +1,34 @@
+"use client"
+import {FormEvent} from 'react'
+import axios from "axios";
 
 export default function Page() {
+
+    const url = process.env.NEXT_PUBLIC_API_URL
+
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+
+        console.log("formData", formData.get('email'), formData.get('password'))
+
+        axios.post(url + '/api/auth/login', {
+            email: formData.get('email'),
+            password: formData.get('password')
+        })
+            .then((response) => {
+                console.log("response", response);
+            })
+            .catch((error) => {
+                console.log("error", error);
+            })
+    }
+
     return (
-        <form>
-            <input type="email" name="email" placeholder="Email" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <button type="submit">Login</button>
+        <form onSubmit={onSubmit}>
+            <input type="email" name="email"/>
+            <input type="password" name="password"/>
+            <button type="submit">Submit</button>
         </form>
     )
 }
