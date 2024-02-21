c11af01b360e0a7143354a8c108fc41b590142f6
Oleg Tkach <KratosHome101@gmail.com>
2024-02-21 08:50:25 +0200

    correct bag

--- src/components/client/PopularCategories/PopularCategories.tsx
+++ src/components/client/PopularCategories/PopularCategories.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {FC, useEffect, useRef} from 'react';
+import React, {FC, useRef} from 'react';
 import './PopularCategories.scss';
 import gsap from 'gsap';
 import {useGSAP} from "@gsap/react";
