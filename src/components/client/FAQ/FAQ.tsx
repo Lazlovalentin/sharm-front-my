7cab19f0a95e9b4fe5e5708f41f44c17917b7102
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-03-04 14:14:23 +0100

    feat: add styles for FAQ

--- src/components/client/FAQ/FAQ.tsx
+++ src/components/client/FAQ/FAQ.tsx
@@ -1,72 +1,68 @@
-'use client';
-import {useRef} from 'react';
-import "./FAQ.scss"
-import {gsap} from 'gsap';
-import {faqSectionData} from "@/mokData/faqSection";
-import {useGSAP} from "@gsap/react";
+"use client";
+import { useRef } from "react";
+import "./FAQ.scss";
+import { gsap } from "gsap";
+import { faqSectionData } from "@/mokData/faqSection";
+import { useGSAP } from "@gsap/react";
 import Accordion from "@/components/UI/Accordion/Accordion";
 
 const Faq = () => {
-    const sectionRef = useRef<HTMLDivElement>(null);
+  const sectionRef = useRef<HTMLDivElement>(null);
 
-    useGSAP(() => {
-        let tl = gsap.timeline({
-            autoAlpha: 0,
-            scrollTrigger: {
-                trigger: sectionRef.current,
-                start: 'top center+=200',
-                end: 'bottom+=150 center-=100',
-                toggleActions: 'play reverse play reverse',
-                // markers: true,
-            },
-        });
-
-        tl.to(sectionRef.current, {
-            id: 'faqSection',
-            duration: 0.3,
-            autoAlpha: 1,
-            ease: 'none',
-        }).to(`button`, {
-            x: 0,
-            stagger: 0.2,
-        });
+  useGSAP(() => {
+    let tl = gsap.timeline({
+      autoAlpha: 0,
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: "top center+=200",
+        end: "bottom+=150 center-=100",
+        toggleActions: "play reverse play reverse",
+        // markers: true,
+      },
     });
 
-    return (
-        <section className="container-main-faq" ref={sectionRef}>
-            <h5 className="title-main-faq">FAQ</h5>
-            <ul className='wrapper-list-faq'>
-                <li>
-                    <Accordion
-                        title={faqSectionData[0].question}
-                        style={{transform: 'translate(-1000px, 0px)'}}
-                    >
-                        {faqSectionData[0].answer}
-                    </Accordion>
-                    <Accordion
-                        title={faqSectionData[1].question}
-                        style={{transform: 'translate(-1000px, 0px)'}}
-                    >
-                        {faqSectionData[1].answer}
-                    </Accordion>
-                </li>
-                <li>
-                    <Accordion
-                        title={faqSectionData[2].question}
-                        style={{transform: 'translate(1000px, 0px)'}}
-                    >
-                        {faqSectionData[2].answer}
-                    </Accordion>
-                    <Accordion
-                        title={faqSectionData[3].question}
-                        style={{transform: 'translate(1000px, 0px)'}}
-                    >
-                        {faqSectionData[3].answer}
-                    </Accordion>
-                </li>
-            </ul>
-        </section>
-    );
+    tl.to(sectionRef.current, {
+      id: "faqSection",
+      duration: 0.3,
+      autoAlpha: 1,
+      ease: "none",
+    }).to(`button`, {
+      x: 0,
+      stagger: 0.2,
+    });
+  });
+
+  return (
+    <section className="container-main-faq" ref={sectionRef}>
+      <h5 className="title-main-faq">FAQ</h5>
+      <ul className="wrapper-list-faq">
+        <li>
+          <Accordion
+            title={faqSectionData[0].question}
+            style={{ transform: "translate(-1000px, 0px)" }}>
+            {faqSectionData[0].answer}
+          </Accordion>
+          <Accordion
+            title={faqSectionData[1].question}
+            style={{ transform: "translate(-1000px, 0px)" }}>
+            {faqSectionData[1].answer}
+          </Accordion>
+        </li>
+        <li>
+          <Accordion
+            title={faqSectionData[2].question}
+            style={{ transform: "translate(1000px, 0px)" }}>
+            {faqSectionData[2].answer}
+          </Accordion>
+          <Accordion
+            title={faqSectionData[3].question}
+            style={{ transform: "translate(1000px, 0px)" }}>
+            {faqSectionData[3].answer}
+          </Accordion>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Faq;
