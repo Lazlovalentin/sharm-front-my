422d0da4845b601e9f7309a3dc6a7101198aa209
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-03-28 13:27:03 +0100

    feat: add animations

--- src/components/client/Testimonials/Testimonials.tsx
+++ src/components/client/Testimonials/Testimonials.tsx
@@ -1,7 +1,7 @@
 "use client";
 import StarRating from "@/components/general/RaitingStars/RatingStars";
 import "./Testimonials.scss";
-import React, { FC, useRef, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import { TestimonialsItem } from "@/mokData/testimonialsData";
 import { ArrowLong } from "@/components/svg/Arrow/Arrow-long";
 import { useGSAP } from "@gsap/react";
@@ -14,16 +14,39 @@ interface TestimonialsProps {
 
 const Testimonials: FC<TestimonialsProps> = ({ data }) => {
   let itemsList = useRef<HTMLUListElement | null>(null);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
   const itemsPerPage = 3;
   const [offset, setOffset] = useState<number>(0);
   const [startReached, setStartReached] = useState<boolean>(true);
   const [endReached, setEndReached] = useState<boolean>(false);
+  useEffect(() => {
+    if (itemsList.current) {
+      gsap.fromTo(
+        itemsList.current.children,
+        {
+          autoAlpha: 0,
+          y: 70,
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 0.8,
+          ease: "none",
+          stagger: 0.5,
+          scrollTrigger: {
+            trigger: itemsList.current,
+            toggleActions: "play reverse play reverse",
+          },
+        }
+      );
+    }
+  }, [data]);
+
   useGSAP(
     () => {
       const shift =
         itemsList.current?.children[0]?.getBoundingClientRect().width || 0;
-      console.log("shift", shift);
-
       Array.from(itemsList.current?.children || []).forEach((el, _) => {
         gsap.to(el as HTMLElement, {
           x: -(shift * offset),
@@ -34,30 +57,31 @@ const Testimonials: FC<TestimonialsProps> = ({ data }) => {
     },
     { dependencies: [offset] }
   );
+
+  useEffect(() => {
+    setStartReached(offset === 0);
+  }, [offset]);
+
+  useEffect(() => {
+    setEndReached(offset >= data.length - itemsPerPage);
+  }, [offset, data]);
+
   const handleArrowClick = (direction: "next" | "prev") => {
     setOffset((prevOffset) => {
       let newOffset =
         prevOffset + (direction === "next" ? itemsPerPage : -itemsPerPage);
-      console.log("newOffset", newOffset);
 
       if (newOffset < 0) {
         newOffset = 0;
-        setStartReached(true);
-        setEndReached(false);
       } else if (newOffset >= data.length) {
         newOffset = data.length - itemsPerPage;
-        console.log("newOffset else if", newOffset);
-        console.log("data.length", data.length);
-
-        setStartReached(false);
-        setEndReached(true);
       }
       return newOffset;
     });
   };
 
   return (
-    <section className="container-testimonials">
+    <section className="container-testimonials" ref={sectionRef}>
       <h5 className="title-testimonials">Відгуки наших покупців</h5>
       <ul className="main-testimonials-wrapper" ref={itemsList}>
         {data.map((item) => (
