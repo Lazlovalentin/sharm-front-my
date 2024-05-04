201709f7c542a669c2df85dda56a853ab87430a7
Valentyn Andreishyn <145656422+Lazlovalentin@users.noreply.github.com>
2024-05-04 14:10:09 +0300

    Update Header.tsx

--- src/components/client/Header/Header.tsx
+++ src/components/client/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from "@/components/client/Header/NavBar/NavBar";
-import "./Header.scss"
+import "/Header.scss"
 import TopBarHeader from "@/components/client/Header/TopBarHeader/TopBarHeader";
 import MainHeader from "@/components/client/Header/MainHeader/MainHeader";
 
